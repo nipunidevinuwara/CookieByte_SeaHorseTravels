b37c8a6d3fdc66f5168ff5e70f6b60e4330357e0
IT21467448 <100064873+IT21467448@users.noreply.github.com>
2023-08-18 23:24:23 +0530

    Booking Step 01 of Mobile app

--- app/src/main/java/com/example/seahorsetravelsapp/MainActivity.kt
+++ app/src/main/java/com/example/seahorsetravelsapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.seahorsetravelsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.seahorsetravelsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initUI()
+    }
+
+    private fun initUI() {
+        binding.customToolBar.number1.setOnClickListener {
+            Toast.makeText(this, "Number 1 Clicked", Toast.LENGTH_SHORT).show()
+        }
+        binding.customToolBar.number2.setOnClickListener {
+            Toast.makeText(this, "Number 12Clicked", Toast.LENGTH_SHORT).show()
+        }
+        binding.customToolBar.number3.setOnClickListener {
+            Toast.makeText(this, "Number 3 Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
